8097bd65b40480aa5af13e1276fb1499b11f2d83
AkshatJha21 <akshatj2134@gmail.com>
2024-10-20 15:44:26 +0530

    dashboard user details

--- src/components/page/Dashboard.tsx
+++ src/components/page/Dashboard.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Navbar from "./Navbar"
 
 const Dashboard = () => {
   return (
-    <div className=''>
-        
+    <div className='mx-6'>
+        <Navbar />
+        <div className="h-screen w-full p-6">
+            <div className="flex-col">
+                <p className="text-2xl font-medium">Welcome,</p>
+                <h1 className="text-4xl font-bold">John Doe</h1>
+                <div className="flex-col my-4 bg-zinc-100 w-[240px] p-4 rounded-md shadow-md">   
+                    <div className="flex space-x-4 items-baseline">
+                        <p className="text-sm text-slate-500">Investor ID</p>
+                        <p className="text-lg font-medium">12</p>
+                    </div>
+                    <div className="flex space-x-4 items-baseline">
+                        <p className="text-sm text-slate-500">Registered Territory</p>
+                        <p className="text-lg font-medium">B</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
--- src/components/page/Navbar.tsx
+++ src/components/page/Navbar.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import { Button } from "../ui/button"
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='w-full p-6'>
+        <div className='flex items-center justify-end space-x-6'>
+            <Link to={'/signin-investor'}>
+                <Button variant={'destructive'}>
+                    Logout
+                </Button>
+            </Link>
+        </div>
+    </div>
   )
 }
 
